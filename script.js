fe7455ceccba8b6036ed0b5bf0cffa094c46ac5a
sidrarana0 <sidrarana703@gmail.com>
2024-06-26 09:30:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#box")
+elemC.addEventListener("mouseenter",function(){
+    fixed.style.display="block"
+})
+elemC.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+
+})
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+
+    
+})
